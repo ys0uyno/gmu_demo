4356f8f0684e7fffd60789de45888e95c5b1a2d4
ys0uyno <ys0uyno@163.com>
2019-03-23 16:26:09 +0800

    use param '-init', '-update'

--- gmu_demo.go
+++ gmu_demo.go
@@ -10,27 +10,24 @@ import (
 )
 
 const VERSION = "0.0.1"
+const GMU_DEMO_RC = ".gmu_demorc"
 
 var versionFlag *bool = flag.Bool("v", false, "Print the version number.")
 var infoFlag *bool = flag.Bool("info", false,
 	"Get current git configuration information.")
-var saveFlag *bool = flag.Bool("save", false,
-	"Save the current user's git configuration.")
-var saveFlag2 *bool = flag.Bool("save2", false,
-	"Save the current user's git configuration.")
 var restoreFlag1 *bool = flag.Bool("r1", false,
 	"Restore user's git configuration.")
 var restoreFlag2 *bool = flag.Bool("r2", false,
 	"Restore user's git configuration.")
+var initFlag *bool = flag.Bool("init", false, "Initialization")
+var updateFlag *bool = flag.Bool("update", false, "Update")
 
 func get_git_config_info() bool {
 	fmt.Println("get_git_config_info()")
 	var USERPROFILE string = os.Getenv(`USERPROFILE`)
-	fmt.Println("USERPROFILE:", USERPROFILE)
 
 	var git_config_file string = USERPROFILE
 	git_config_file += "\\.gitconfig"
-	fmt.Println(".gitconfig:", git_config_file)
 
 	if utils.FileExist(git_config_file) {
 		fmt.Printf("\"%s\" exists\n", git_config_file)
@@ -52,109 +49,12 @@ func get_git_config_info() bool {
 
 	var ssh_dir string = USERPROFILE
 	ssh_dir += "\\.ssh"
-	fmt.Println(".ssh:", ssh_dir)
 
 	if utils.FileExist(ssh_dir) {
 		fmt.Printf("\"%s\" exists\n", ssh_dir)
 	} else {
 		fmt.Printf("\"%s\" does not exist\n", ssh_dir)
 		return false;
-	}
-
-	return true;
-}
-
-func save_git_config() bool {
-	if !get_git_config_info() {
-		fmt.Println("The current git configuration may be incomplete.")
-		return false;
-	}
-
-	var USERPROFILE string = os.Getenv(`USERPROFILE`)
-	var git_config_file string = USERPROFILE
-	git_config_file += "\\.gitconfig"
-	var git_config_file_new string = USERPROFILE
-	git_config_file_new += "\\.gitconfig.1"
-
-	ret, _ := utils.CopyFile(git_config_file_new, git_config_file)
-	if ret == 0 {
-		// return false;
-	}
-
-	var ssh_dir string = USERPROFILE
-	ssh_dir += "\\.ssh"
-	var ssh_dir_new string = USERPROFILE
-	ssh_dir_new += "\\.ssh.1"
-
-	fmt.Printf("%s\n", ssh_dir)
-	fmt.Printf("%s\n", ssh_dir_new)
-
-	err := os.MkdirAll(ssh_dir_new, os.ModePerm)
-	if err != nil {
-		fmt.Println("failed")
-	} else {
-		fmt.Println("succeed")
-	}
-
-	files, err := ioutil.ReadDir(ssh_dir)
-	if err != nil {
-		fmt.Println("ReadDir failed")
-	}
-
-	for _, f := range files {
-		var f_old string = ssh_dir + "\\" + f.Name()
-		var f_new string = ssh_dir_new + "\\" + f.Name()
-		fmt.Println(f_old)
-		fmt.Println(f_new)
-		utils.CopyFile(f_new, f_old)
-	}
-
-	return true;
-}
-
-func save_git_config2() bool {
-	if !get_git_config_info() {
-		fmt.Println("The current git configuration may be incomplete.")
-		return false;
-	}
-
-	var USERPROFILE string = os.Getenv(`USERPROFILE`)
-	var git_config_file string = USERPROFILE
-	git_config_file += "\\.gitconfig"
-	var git_config_file_new string = USERPROFILE
-	git_config_file_new += "\\.gitconfig.2"
-
-	ret, _ := utils.CopyFile(git_config_file_new, git_config_file)
-	if ret == 0 {
-		// return false;
-	}
-
-	var ssh_dir string = USERPROFILE
-	ssh_dir += "\\.ssh"
-	var ssh_dir_new string = USERPROFILE
-	ssh_dir_new += "\\.ssh.2"
-
-	fmt.Printf("%s\n", ssh_dir)
-	fmt.Printf("%s\n", ssh_dir_new)
-
-	err := os.MkdirAll(ssh_dir_new, os.ModePerm)
-	if err != nil {
-		fmt.Println("failed")
-	} else {
-		fmt.Println("succeed")
-	}
-
-	files, err := ioutil.ReadDir(ssh_dir)
-	if err != nil {
-		fmt.Println("ReadDir failed")
-	}
-
-	for _, f := range files {
-		var f_old string = ssh_dir + "\\" + f.Name()
-		var f_new string = ssh_dir_new + "\\" + f.Name()
-		fmt.Println(f_old)
-		fmt.Println(f_new)
-		utils.CopyFile(f_new, f_old)
 	}
 
 	return true;
@@ -256,6 +156,135 @@ func restore_config2() bool {
 	return true;
 }
 
+func get_current_git_user() string {
+	var USERPROFILE string = os.Getenv(`USERPROFILE`)
+
+	var git_config string = USERPROFILE
+	git_config += "\\.gitconfig"
+
+	if !utils.FileExist(git_config) {
+		fmt.Printf("\"%s\" does not exist\n", git_config)
+		return "";
+	}
+
+	cfg, err := ini.Load(git_config)
+	if err != nil {
+		fmt.Printf("Fail to read file: %v", err)
+		return "";
+	}
+
+	var name string = cfg.Section("user").Key("name").String()
+	// var email string = cfg.Section("user").Key("email").String()
+
+	return name
+}
+
+func save_git_config(home, user string) bool {
+	current_git_user := get_current_git_user()
+	old_config_file := home + "\\.gitconfig"
+	new_config_file := old_config_file + "." + user
+
+	// if current user is equal to 'user' and .gitconfig.user exists,
+	// no need save again
+	if current_git_user == user && utils.FileExist(new_config_file) {
+		fmt.Printf("\"%s\" already exists.\n", new_config_file)
+		return true;
+	}
+
+	// save .gitconfig as .gitconfig.user_name
+	ret, _ := utils.CopyFile(new_config_file, old_config_file)
+	if ret == 0 {
+		fmt.Println("utils.CopyFile failed")
+		return false
+	}
+
+	return true
+}
+
+func save_ssh_config(home, user string) bool {
+	current_git_user := get_current_git_user()
+	old_config_file := home + "\\.ssh"
+	new_config_file := old_config_file + "." + user
+
+	// if current user is equal to 'user' and .ssh.user exists,
+	// no need save again
+	if current_git_user == user && utils.FileExist(new_config_file) {
+		fmt.Printf("\"%s\" already exists.\n", new_config_file)
+		return true;
+	}
+
+	err := os.MkdirAll(new_config_file, os.ModePerm)
+	if err != nil {
+		fmt.Println("os.MkdirAll failed")
+		return false
+	} else {
+		fmt.Println("os.MkdirAll succeeded")
+	}
+
+	files, err := ioutil.ReadDir(old_config_file)
+	if err != nil {
+		fmt.Println("ReadDir failed")
+		return false
+	}
+
+	for _, f := range files {
+		var f_old string = old_config_file + "\\" + f.Name()
+		var f_new string = new_config_file + "\\" + f.Name()
+		fmt.Println(f_old)
+		fmt.Println(f_new)
+		utils.CopyFile(f_new, f_old)
+	}
+
+	return true
+}
+
+func init_env() bool {
+	var USERPROFILE string = os.Getenv(`USERPROFILE`)
+
+	var git_config_file string = USERPROFILE
+	git_config_file += "\\.gitconfig"
+
+	if !utils.FileExist(git_config_file) {
+		fmt.Printf("\"%s\" does not exist\n", git_config_file)
+		return false;
+	}
+
+	var ssh_dir string = USERPROFILE
+	ssh_dir += "\\.ssh"
+
+	if !utils.FileExist(ssh_dir) {
+		fmt.Printf("\"%s\" does not exist\n", ssh_dir)
+		return false;
+	}
+
+	cfg, err := ini.Load(git_config_file)
+	if err != nil {
+		fmt.Printf("Fail to read file: %v\n", err)
+		return false;
+	}
+
+	var user_name string = cfg.Section("user").Key("name").String()
+	var user_email string = cfg.Section("user").Key("email").String()
+
+	fmt.Printf("user.name: %s, user.email: %s\n", user_name, user_email)
+
+	if !save_git_config(USERPROFILE, user_name) {
+		fmt.Println("save .gitconfig failed.")
+		return false
+	}
+
+	if !save_ssh_config(USERPROFILE, user_name) {
+		fmt.Println("save .ssh failed.")
+		return false
+	}
+
+	return true;
+}
+
+func update_env() bool {
+	return init_env()
+}
+
 func main() {
 	flag.Parse()
 
@@ -263,18 +292,6 @@ func main() {
 		fmt.Println("version:", VERSION)
 	} else if *infoFlag {
 		get_git_config_info()
-	} else if *saveFlag {
-		if !save_git_config() {
-			fmt.Println("save git config failed")
-		} else {
-			fmt.Println("save git config succeeded")
-		}
-	} else if *saveFlag2 {
-		if !save_git_config2() {
-			fmt.Println("save git config failed")
-		} else {
-			fmt.Println("save git config succeeded")
-		}
 	} else if *restoreFlag1 {
 		if !restore_config1() {
 			fmt.Println("restore git config failed")
@@ -287,5 +304,13 @@ func main() {
 		} else {
 			fmt.Println("restore git config succeeded")
 		}
+	} else if *initFlag {
+		fmt.Println("Init...")
+		init_env()
+	} else if *updateFlag {
+		fmt.Println("Update...")
+		update_env()
+	} else {
+		fmt.Println("Print usage.")
 	}
 }
